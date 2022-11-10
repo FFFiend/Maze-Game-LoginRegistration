14511102d8915200272e888597a22a5a7ef5527b
Rene Koike <rene.koike@mail.utoronto.ca>
2022-11-10 18:47:18 -0500

    Changed final variables names

--- src/main/java/adapters/default_game/GamePanelPresenter.java
+++ src/main/java/adapters/default_game/GamePanelPresenter.java
@@ -9,19 +9,19 @@ import java.awt.*;
  * A class responsible for what the GamePanelUI presents
  **/
 public class GamePanelPresenter extends JPanel implements IGamePanelPresenterUpdater {
-    final int originalTileSize = 16;
-    final int scale = 3;
-    final int tileSize = originalTileSize * scale;
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol;
-    final int screenHeight = tileSize * maxScreenRow;
+    final int SPRITE_TILE_SIZE = 16;
+    final int SCALE = 3; // may be changed to an unfixed variable later
+    final int TILE_SIZE = SPRITE_TILE_SIZE * SCALE;
+    final int MAX_PANEL_COL= 16;
+    final int MAX_PANEL_ROW = 12;
+    final int PANEL_WIDTH = TILE_SIZE * MAX_PANEL_COL;
+    final int PANEL_HEIGHT = TILE_SIZE * MAX_PANEL_ROW;
 
     /**
      * Construct a new GamePanelPresenter with fixed settings
      **/
     public GamePanelPresenter() {
-        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
     }
