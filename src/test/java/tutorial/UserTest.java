eebb95b371ae7376234bdb28cc596d6aa9ae7f66
arifa <arifa.mansoor@mail.utoronto.ca>
2022-11-11 16:25:19 -0500

    Empty files for the login, leaderboard, and register features.

--- src/test/java/tutorial/UserTest.java
+++ src/test/java/tutorial/UserTest.java
@@ -5,4 +5,35 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.Test;
 
 public class UserTest {
+
+    @Test
+    public void GetPassword(){
+        User testUser = new User("Owais","1234");
+        Assertions.assertEquals("1234",testUser.getPassWord());
+    }
+
+
+    @Test
+    public void GetUsername(){
+        User testUser = new User("Zahid","TorontoRaptors");
+        Assertions.assertEquals("Zahid",testUser.getUserName());
+
+    }
+
+
+    @Test
+    public void GetUnchangedScore(){
+        User testUser = new User("Owais","1234");
+        Assertions.assertEquals(0,testUser.getEscore());
+        Assertions.assertEquals(0,testUser.getMscore());
+        Assertions.assertEquals(0,testUser.getHscore());
+    }
+
+
+    @Test
+    public void GetChangedScore(){
+        User primeMinister = new User("Justin Trudeau", "Canada");
+        primeMinister.setEscore(50);
+        Assertions.assertEquals(50, primeMinister.getEscore());
+    }
 }
