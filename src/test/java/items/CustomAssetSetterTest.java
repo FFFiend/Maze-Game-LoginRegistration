576558e9c975707016b35d43e0c30a8fdcee0c39
tseamuscorlett <t.seamus.corlett@gmail.com>
2022-11-12 16:21:08 -0500

    Added get & remove methods for MazeItems
    Added CustomAssetSetter for setting Assets with maze.txt input

--- src/test/java/items/CustomAssetSetterTest.java
+++ src/test/java/items/CustomAssetSetterTest.java
@@ -18,10 +18,10 @@ public class CustomAssetSetterTest {
         CustomAssetSetter caSetter = new CustomAssetSetter("maze01.txt", mazeItems, mapHazards);
 
         Assertions.assertEquals(0, caSetter.getMazeAssetNum()[0][0]);
-        Assertions.assertEquals(0, caSetter.getMazeAssetNum()[12][10]);
-        Assertions.assertNull(caSetter.getMazeItems().get(0, 0));
-        Assertions.assertNull(caSetter.getMazeHazards().getEnemy(5, 5));
+        Assertions.assertNotEquals(4, caSetter.getMazeAssetNum()[12][10]);
+        Assertions.assertNull(caSetter.getMazeHazards().getEnemy(0, 0));
         Assertions.assertNull(caSetter.getMazeHazards().getObstacle(7, 7));
+        Assertions.assertNull(caSetter.getMazeItems().get(3, 3));
     }
 
     /** Test CustomAssetSetter with at least one of each integer. */
@@ -47,9 +47,9 @@ public class CustomAssetSetterTest {
         MazeHazards mapHazards = new MazeHazards();
         CustomAssetSetter caSetter = new CustomAssetSetter("maze03.txt", mazeItems, mapHazards);
 
-        Assertions.assertEquals(5, caSetter.getMazeAssetNum()[0][5]);
-        Assertions.assertEquals(5, caSetter.getMazeAssetNum()[1][5]);
-        Assertions.assertEquals(5, caSetter.getMazeAssetNum()[2][5]);
+        Assertions.assertEquals(5, caSetter.getMazeAssetNum()[5][0]);
+        Assertions.assertEquals(5, caSetter.getMazeAssetNum()[5][1]);
+        Assertions.assertEquals(5, caSetter.getMazeAssetNum()[5][2]);
         Assertions.assertNull(caSetter.getMazeHazards().getEnemy(0, 5));
         Assertions.assertNull(caSetter.getMazeHazards().getObstacle(1, 5));
         Assertions.assertNotNull(caSetter.getMazeItems().get(1, 5));
