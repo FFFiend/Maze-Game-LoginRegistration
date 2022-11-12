4b55fa0401de369f0dde0203ddcdf6486f6bd4f8
arifa <arifa.mansoor@mail.utoronto.ca>
2022-11-12 18:59:56 -0500

    Pulled the new changes from main.

--- src/main/java/entities/login_leaderboard/User.java
+++ src/main/java/entities/login_leaderboard/User.java
@@ -1,7 +1,7 @@
 package entities.login_leaderboard;
 
 /**
- * The user stores a user's username, password, email, and the quickest
+ * The user stores a user's name, password, email, and the quickest
  * easy, medium, hard scores.
  */
 public class User {
