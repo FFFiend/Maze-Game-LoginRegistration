ef4ccc33956904a7391ce39107c653848469efc8
tseamuscorlett <t.seamus.corlett@gmail.com>
2022-11-21 23:47:13 -0500

    changing numbers in maze02.txt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,7 @@ public class Main {
      */
     public static void main(String[] args) {
         setupGame();
-        //setupCustomMazeMenu();
+        setupCustomMazeMenu();
     }
 
     /**
@@ -28,7 +28,7 @@ public class Main {
     private static void setupGame() {
         GlobalFrame globalFrame = new GlobalFrame();
         globalFrame.setWelcomePanel();
-        //tempDefaultGameRunner();
+        tempDefaultGameRunner();
 
     }
 
