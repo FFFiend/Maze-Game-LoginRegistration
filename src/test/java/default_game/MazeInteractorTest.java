9962512efcb926cdcd54582dfd0ab5877908b279
tseamuscorlett <t.seamus.corlett@gmail.com>
2022-12-08 03:29:23 -0500

    Autograding can't locate .wav files so took out TestPickUpItem in CollisionHandlerTest.
    Instead added more tests in MazeInteractorTest

--- src/test/java/default_game/MazeInteractorTest.java
+++ src/test/java/default_game/MazeInteractorTest.java
@@ -38,4 +38,38 @@ public class MazeInteractorTest {
         mazeInteractor.execute(KeyEvent.VK_R);
         Assertions.assertFalse(mazeInteractor.gameOver());
     }
+
+    /**
+     * Test that gameOver is true when Player is out of Stamina
+     */
+    @Test
+    public void TestStaminaOut() {
+        IGamePanelOutputBoundary outputBoundary = new TestGamePanelOutputBoundaryModel();
+        MazeInteractor mazeInteractor = new MazeInteractor(outputBoundary);
+        mazeInteractor.load("maze04.txt");
+        mazeInteractor.startGameThread();
+        mazeInteractor.execute(KeyEvent.VK_D);
+        int i = 0;
+        while (i < 24) {
+            mazeInteractor.execute(KeyEvent.VK_S);
+            mazeInteractor.execute(KeyEvent.VK_W);
+            i++;
+        }
+        Assertions.assertFalse(mazeInteractor.gameOver());
+
+        // take the last step
+        mazeInteractor.execute(KeyEvent.VK_S);
+        Assertions.assertTrue(mazeInteractor.gameOver());
+    }
+
+    /**
+     * Test that displayLevel loads the maze
+     */
+    @Test
+    public void DisplayLevel() {
+        IGamePanelOutputBoundary outputBoundary = new TestGamePanelOutputBoundaryModel();
+        MazeInteractor mazeInteractor = new MazeInteractor(outputBoundary);
+        mazeInteractor.displayLevel(KeyEvent.VK_1);
+        Assertions.assertEquals(50, mazeInteractor.getPlayerStamina());
+    }
 }
